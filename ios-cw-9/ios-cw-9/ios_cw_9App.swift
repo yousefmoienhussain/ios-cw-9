e4c44b85fd639d6b5814698e681497a54a75503b
Retajalotaibi <rtooguae@gmail.com>
2022-03-09 02:47:23 +0300

    done

---
+++ ios-cw-9/ios-cw-9/ios_cw_9App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_cw_9App.swift
+//  ios-cw-9
+//
+//  Created by Retaj Al-Otaibi on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_cw_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
