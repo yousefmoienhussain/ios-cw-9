fad16e48c50acb9d4f1ff6bbd06337fea9880fd9
shaimaa1236 <60436597+shaimaa1236@users.noreply.github.com>
2022-03-09 16:54:14 +0300

    .

--- ios-cw-9/ios-cw-9/ContentView.swift
+++ ios-cw-9/ios-cw-9/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var grade = ""
+    @State var enterGrade = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.black)
+                .opacity(0.3)
+                .ignoresSafeArea()
+            VStack{
+                Text("حاسبة الدرجات")
+                    .bold()
+                    .font(.largeTitle)
+                
+                TextField("ادخل درجتك", text: $enterGrade)
+                    .background(.white)
+                    .padding()
+                    .foregroundColor(.black)
+                
+                Text("احسب درجتي")
+                    .padding()
+                    .background(Color(#colorLiteral(red: 0.2379182279, green: 0.3681719899, blue: 0.7421068549, alpha: 1)))
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .onTapGesture {
+                        // enter the code here  اكتب الكود هنا
+                        // if statement
+                        // here يجب أن تتغير الصورة على حسب الدرجة
+                    }
+                
+                Text("لقد حصلت على درجة")
+                    .bold()
+                    .font(.title2)
+                    .padding()
+                Image(grade)
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+            }
+            .foregroundColor(.white)
+        }
     }
 }
 
