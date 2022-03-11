f09277329e495c55cc29d791c2fe142cab03fc0e
shaimaa1236 <60436597+shaimaa1236@users.noreply.github.com>
2022-03-11 23:00:14 +0300

    .

--- ios-cw-9/ios-cw-9/ContentView.swift
+++ ios-cw-9/ios-cw-9/ContentView.swift
@@ -12,31 +12,39 @@ struct ContentView: View {
     @State var enterGrade = ""
     var body: some View {
         ZStack {
-            Color(.black)
-                .opacity(0.3)
+            Color(#colorLiteral(red: 0.6411222816, green: 0.8151980042, blue: 0.8953691125, alpha: 1))
                 .ignoresSafeArea()
             VStack{
                 Text("حاسبة الدرجات")
                     .bold()
                     .font(.largeTitle)
-                
-                TextField("ادخل درجتك", text: $enterGrade)
-                    .background(.white)
+                Image("cal")
+                    .resizable()
+                    .scaledToFit()
                     .padding()
+                TextField("ادخل درجتك", text: $enterGrade)
+                    .padding()
+                    .background(.white)
                     .foregroundColor(.black)
+                    .padding()
                 
                 Text("احسب درجتي")
+                    .bold()
+                    .foregroundColor(Color(#colorLiteral(red: 0.07661154121, green: 0.1822937131, blue: 0.2113724947, alpha: 1)))
                     .padding()
-                    .background(Color(#colorLiteral(red: 0.2379182279, green: 0.3681719899, blue: 0.7421068549, alpha: 1)))
+                    .background(Color(#colorLiteral(red: 0.975943625, green: 0.7875768542, blue: 0, alpha: 1)))
                     .clipShape(RoundedRectangle(cornerRadius: 25))
                     .onTapGesture {
                         // enter the code here  اكتب الكود هنا
                         // if statement
                         // here يجب أن تتغير الصورة على حسب الدرجة
+                        // image : الصور موجودة في assets
+                        
                     }
                 
                 Text("لقد حصلت على درجة")
                     .bold()
+                    .foregroundColor(Color(#colorLiteral(red: 0.07661154121, green: 0.1822937131, blue: 0.2113724947, alpha: 1)))
                     .font(.title2)
                     .padding()
                 Image(grade)
